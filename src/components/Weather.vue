a915e1f8ae3c737ebcc2821e10d00631fec8871b
Timen Engwerda <tengwerda@gmail.com>
2017-07-12 20:30:55 +0200

    Add wind to Weather component

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,6 +3,11 @@
     <div class="weather__temperature">{{ tempFixed }}<span class="weather__temperature-degrees">&#8451;</span></div>
     <div class="weather__city">{{ cityName }} </div>
     <div :class="weatherIcon" class="weather__icon" v-if="icon !== ''"></div>
+    <div class="weather__wind">
+      <div class="weather__wind-bft">{{ beaufort }}BFT</div>
+      <div class="weather__wind-icon"></div>
+      <i class="fa fa-arrow-up" :style="windDirection"></i>
+    </div>
   </div>
 </template>
 
@@ -16,7 +21,9 @@ export default {
       cityId: '2751791',
       currentTemperature: 0,
       cityName: '',
-      icon: ''
+      icon: '',
+      windSpeed: 0,
+      windDirectionDegrees: ''
     }
   },
   computed: {
@@ -25,6 +32,43 @@ export default {
     },
     tempFixed () {
       return this.currentTemperature.toFixed(1)
+    },
+    beaufort () {
+      console.log(this.windSpeed)
+      if (this.windSpeed <= 0.2) {
+        return 0
+      }
+
+      if (this.windSpeed <= 1.5) {
+        return 1
+      }
+
+      if (this.windSpeed <= 3.3) {
+        return 2
+      }
+
+      if (this.windSpeed <= 5.4) {
+        return 3
+      }
+
+      if (this.windSpeed <= 7.9) {
+        return 4
+      }
+
+      if (this.windSpeed <= 10.7) {
+        return 5
+      }
+
+      if (this.windSpeed <= 13.8) {
+        return 6
+      }
+
+      return '6+'
+    },
+    windDirection () {
+      // the wind direction given by the API is where it's coming from. We want the arrow to point where its going to
+      // so add a 180 degrees to flip it
+      return `transform: rotate(${this.windDirectionDegrees + 180}deg)`
     }
   },
   created () {
@@ -39,6 +83,9 @@ export default {
           this.currentTemperature = r.main.temp
           this.cityName = r.name
           this.icon = r.weather[0].icon
+
+          this.windSpeed = r.wind.speed
+          this.windDirectionDegrees = r.wind.deg
         }
       }, response => {
         // error callback

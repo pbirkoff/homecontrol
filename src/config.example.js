a22b56400079a1a496c27420c8070f560e6813ab
Timen Engwerda <tengwerda@gmail.com>
2017-07-13 10:36:59 +0200

    Add example config

---
+++ src/config.example.js
@@ -0,0 +1,6 @@
+// Rename to config.js and you're good to go
+
+export const weatherApiKey = '' // Openweathermap
+export const hueUrl = ''
+export const wunderListAccessToken = ''
+export const wunderListClientId = ''

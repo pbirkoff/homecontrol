0bd7847883e11ed4abfcc01ec50f1036fdea22b5
Timen Engwerda <tengwerda@gmail.com>
2017-07-27 17:17:18 +0200

    Second try to get MS api to run

--- src/components/MicrosoftToDo/MicrosoftToDo.vue
+++ src/components/MicrosoftToDo/MicrosoftToDo.vue
@@ -18,36 +18,70 @@ export default {
       redirectUri: '',
       userAgentApplication: null,
       token: null
+      // token: (localStorage.getItem('token')) ? localStorage.getItem('token') : null
     }
   },
   methods: {
     init () {
-      const self = this
-      // this.userAgentApplication = new Msal.UserAgentApplication(this.clientID)
-      this.userAgentApplication = new Msal.UserAgentApplication(this.clientID, null, function (errorDes, token, error, tokenType) {
-        // this callback is called after loginRedirect OR acquireTokenRedirect (not used for loginPopup/aquireTokenPopup)
-        console.log('acquire token?')
-      })
+      if (!this.token) {
+        const self = this
+        // this.userAgentApplication = new Msal.UserAgentApplication(this.clientID)
+        this.userAgentApplication = new Msal.UserAgentApplication(this.clientID, null, function (errorDes, token, error, tokenType) {
+          // this callback is called after loginRedirect OR acquireTokenRedirect (not used for loginPopup/aquireTokenPopup)
+          console.log('acquire token?')
+        })
 
-      this.userAgentApplication.loginPopup(['Tasks.read']).then(function (token) {
-        let user = self.userAgentApplication.getUser()
-        if (user) {
-          self.token = token
-          console.log('ssuccess')
-          self.getTasks()
-        }
-        console.log('below if')
-      }, function (error) {
-        // handle error
-        console.log(error)
-      })
+        this.userAgentApplication.loginPopup(['Tasks.readwrite']).then(function (token) {
+          let user = self.userAgentApplication.getUser()
+          if (user) {
+            self.token = token
+            localStorage.setItem('token', token)
+            self.getTasks()
+          }
+          console.log('below if')
+        }, function (error) {
+          // handle error
+          console.log(error)
+        })
+      } else {
+        this.getTasks()
+      }
     },
     getTasks () {
-      const header = 'Bearer ' + this.token
-      this.$http.headers.common['Authorization'] = header
-      console.log(header)
-      this.$http.get('https://graph.microsoft.com/v1.0/me').then(response => {
-        console.log(response)
+      // // this.$http.headers.common['Authorization'] = header
+      // console.log(header)
+      // this.$http.get('https://outlook.office.com/api/v2.0/me/tasks', { headers: { 'Authorization': header } }).then(response => {
+      //   console.log(response)
+      // })
+      const bearer = 'Bearer ' + this.token
+      let headers = new Headers()
+      headers.append('Authorization', bearer)
+      let options = {
+        method: 'GET',
+        headers: headers
+      }
+
+      // Note that fetch API is not available in all browsers
+      fetch('https://outlook.office.com/api/v2.0/me/tasks', options).then(function (response) {
+        let contentType = response.headers.get('content-type')
+        if (response.status === 200 && contentType && contentType.indexOf('application/json') !== -1) {
+          response.json().then(function (data) {
+            console.log(data)
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+        } else {
+          response.json().then(function (data) {
+            console.log(data)
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
       })
     }
   }
